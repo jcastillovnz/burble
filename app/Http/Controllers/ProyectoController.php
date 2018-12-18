3229c5fba98bb28c864f690d8e810c4af79201c3
Jose Castillo <k.y.258@hotmail.com>
2018-12-18 13:52:31 -0300

    UPDATE

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -50,6 +50,50 @@ return response()->json($data);
 
 
 
+public function create( Request $request )
+    {
+
+$proyecto= new Proyectos();
+$proyecto->nombre=  $request->proyecto;
+$proyecto->fecha_entrega=$request->fecha_entrega;
+$proyecto->presupuesto=$request->presupuesto;
+$proyecto->comentario=$request->comentario;
+$proyecto->clientes_id=$request->cliente;
+$proyecto->save();
+
+
+
+if ($proyecto->save()==true) {
+$data = "true";
+return response()->json($data); 
+
+}
+
+
+else {
+
+$data = "false";
+return response()->json($data); 
+
+}
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
