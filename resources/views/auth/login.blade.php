f8c0a6a20b76f04fa8f5fd9ad8e4f7fa400ee091
Jose Castillo <k.y.258@hotmail.com>
2019-02-02 22:34:19 -0300

    UPDATE

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
 
 .login{
 margin: auto;
-max-width: 280px;
+max-width: 65%;
 }
 
 
@@ -22,7 +22,7 @@ max-width: 280px;
 
 .pwd-container{
 
-padding-top: 11%;
+padding-top: 12%;
 
 padding-bottom: 15%;
 
@@ -50,25 +50,33 @@ padding-top: 30%;
 
 <div class="row  pwd-container">
 <div  class="col-md-4"></div>
+
+
+
+
 <div   class="col-md-4"> 
+
+
+
+
 <div class="login">
-<h3>
-<p align="center" class="text-info">
+<p align="center" class="text-info" style="font-size: 25px;">
+  <strong>
 BURBLE
-<strong>
 </strong>
 </p>
-</h3>
 
 <form class="hidden" method="POST" action="{{ route('login') }}">
 @csrf
-<div class="input-group  container-fluid">
-<div class="input-group-prepend">
-<span style="width: 35px"  class="input-group-text">   <i class="fas fa-user"></i>  </span>
-</div>
 
-<input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-   @if ($errors->has('email'))
+
+
+
+<div class="input-group  container-fluid">
+
+<input id="email" type="email" placeholder="Email " class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+@if ($errors->has('email'))
         <span class="invalid-feedback" role="alert">
 <strong>{{ $errors->first('email') }}</strong>
 </span>
@@ -77,9 +85,6 @@ BURBLE
 
 
 <div class="input-group  container-fluid">
-<div class="input-group-prepend">
-<span style="width: 35px"  class="input-group-text">   <i class="fas fa-lock"></i>    </span>
-</div>
 
  <input id="password" placeholder="Contraseña" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 @if ($errors->has('password'))
@@ -88,19 +93,22 @@ BURBLE
 </span>
 @endif
 
-
-
  </div>
 
 
    
+<div class="col-sm-12">
 
-<div class="input-group container-fluid float-right">
-
-<button type="submit" name="go" class="btn btn-info btn-sm rounded float-right">
+<button type="submit"  class="btn btn-info btn-sm  rounded-right float-right">
 <i class="fas fa-fingerprint"></i> Ingresar
 </button>
+
+
 </div>
+
+
+
+
 
 </form>
 </div> <!-- cierre login -->
